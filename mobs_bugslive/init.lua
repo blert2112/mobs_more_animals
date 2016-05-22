69ee73b31c21441417d349ca6804c8251ef6e9ff
blert2112 <cdilts@bellsouth.net>
2016-05-22 00:02:54 -0400

    updated to latest 'mobs.redo'
    also tweaked a few settings

--- mobs_bugslive/init.lua
+++ mobs_bugslive/init.lua
@@ -4,8 +4,24 @@ if mobs.mod and mobs.mod == "redo" then
 -- bugs
 	mobs:register_mob("mobs_bugslive:bug", {
 		type = "animal",
+		--lifetimer = 180,
+
 		visual = "mesh",
+		--visual_size = {x=1, y=1},
 		mesh = "bug.x",
+		--gotten_mesh = "",
+		--rotate = 0,
+		collisionbox = {-0.1, -0.01, -0.1, 0.1, 0.1, 0.1},
+		--animation = { 
+		--	speed_normal = 0,	speed_run = 0,
+		--	stand_start = 0,	stand_end = 0,
+		--	walk_start = 0,		walk_end = 0,
+		--	run_start = 0,		run_end = 0,
+		--	punch_start = 0,	punch_end = 0,
+		--	punch2_start = 0,	punch2_end = 0,
+		--	shoot_start = 0,	shoot_end = 0,
+		--	speed_punch = 0,	speed_punch2 = 0,	speed_shoot = 0
+		--},
 		textures = {
 			{"bug1.png"},
 			{"bug2.png"},
@@ -16,24 +32,86 @@ if mobs.mod and mobs.mod == "redo" then
 			{"bug7.png"},
 			{"bug8.png"}
 		},
-		collisionbox = {-0.2, -0.01, -0.2, 0.2, 0.2, 0.2},
+		--gotten_texture = {{}},
+		--child_texture = {{}},
+
+		stepheight = 1.1,
+		--fear_height = 0,
+		runaway = true,
+		--jump = true,
+		jump_chance = 5,
+		jump_height = 2,
+		fly = false,
+		--fly_in = "air",
+		walk_chance = 60,
+		walk_velocity = 2,
+		run_velocity = 4,
+		fall_speed = -5,
+		floats = 0,
+
+		view_range = 3,
+		--follow = {},
+
+		passive = true,
+		attack_type = "dogfight",
+		damage = 1,
+		reach = 1,
+		--docile_by_day = false,
+		--attacks_monsters = false,
+		pathfinding = false,
+		--double_melee_attack = false,
+		group_attack = true,
+		--explosion_radius = 1,
+		--arrow = "ent:name",
+		--shoot_interval = 1,
+		--shoot_offset = 0,
+		--dogshoot_switch = 1,
+		--dogshoot_count_max = 5,
+
 		hp_min = 1,
 		hp_max = 1,
 		armor = 200,
 		knock_back = 0,
-		blood_amount = 1,
 		lava_damage = 1,
 		fall_damage = 0,
-		damage = 1,
-		reach = 1,
-		attack_type = "dogfight",
-		group_attack = true,
-		view_range = 3,
-		stepheight = 1.1,
-		jump_height = 2,
-		jump_chance = 5,
-		fall_speed = -5,
-		floats = 0,
+		--water_damage = 0,
+		--light_damage = 0,
+		recovery_time = 0.25,
+		--immune_to = {},
+		blood_amount = 1,
+		--blood_texture = "mobs_blood.png",
+
+		--makes_footstep_sound = false,
+		--sounds = {},
+
+		--drops = {},
+
+		replace_what = {
+			"farming:beanpole_5",
+			"farming:cucumber_4",
+			"farming:melon_8",
+			"farming:pumpkin_8"
+		},
+		replace_with = "air",
+		replace_rate = 50,
+		--replace_offset = 0,
+
+		do_custom = function(self, dtime)
+				if math.random() > 0.5 then
+					if self.floats == 0 then
+						self.floats = 1
+					else
+						self.floats = 0
+					end
+				end
+			end
+		--custom_attack = function(self, to_attack)
+			--end,
+		--on_blast = funtion(object, damage)
+				--return do_damage, do_knockback, drops
+			--end,
+		--on_die = function(self, pos)
+			--end,
 		on_rightclick = function(self, clicker)
 			mobs:capture_mob(self, clicker, 50, 80, 0, true, nil)
 		end
