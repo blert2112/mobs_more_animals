b754de45227cdb9fe21dc03d43f877b01d2b2dd6
blert2112 <cdilts@bellsouth.net>
2016-05-25 20:54:48 -0400

    update to latest mobs.redo api

--- mobs_giraffe/init.lua
+++ mobs_giraffe/init.lua
@@ -4,46 +4,106 @@ if mobs.mod and mobs.mod == "redo" then
 -- jeraf
 	mobs:register_mob("mobs_giraffe:jeraf", {
 		type = "animal",
+		--lifetimer = 180,
+
 		visual = "mesh",
+		--visual_size = {x=1, y=1},
 		mesh = "mobs_jeraf.x",
-		textures = {
-			{"mobs_jeraf.png"},
-		},
+		--gotten_mesh = "",
+		--rotate = 0,
 		collisionbox = {-0.4, -0.01, -0.4, 0.4, 1, 0.4},
-		animation = {
+		animation = { 
 			speed_normal = 15,	speed_run = 24,
 			stand_start = 0,	stand_end = 30,
 			walk_start = 70,	walk_end = 100,
 			run_start = 70,		run_end = 100
+			--punch_start = 0,	punch_end = 0,
+			--punch2_start = 0,	punch2_end = 0,
+			--shoot_start = 0,	shoot_end = 0,
+			--speed_punch = 0,	speed_punch2 = 0,	speed_shoot = 0
 		},
-		makes_footstep_sound = true,
+		textures = {
+			{"mobs_jeraf.png"},
+		},
+		--gotten_texture = {},
+		--child_texture = {},
+
+		stepheight = 2.1,
+		fear_height = 3,
+		jump = false,
+		--jump_chance = 0,
+		jump_height = 4,
+		fly = false,
+		--fly_in = "air",
+		walk_chance = 75,
+		walk_velocity = 2,
+		run_velocity = 4,
+		--fall_speed = -10,
+		--floats = 1,
+
+		view_range = 16,
+		follow = {"group:leaves"},
+
+		passive = true,
+		attack_type = "dogfight",
+		damage = 10,
+		reach = 3,
+		--docile_by_day = false,
+		--attacks_monsters = false,
+		runaway = true,
+		pathfinding = false,
+		--double_melee_attack = false,
+		--group_attack = false,
+		--explosion_radius = 1,
+		--arrow = "ent:name",
+		--shoot_interval = 1,
+		--shoot_offset = 0,
+		--dogshoot_switch = 1,
+		--dogshoot_count_max = 5,
+
 		hp_min = 8,
 		hp_max = 12,
 		armor = 200,
 		knock_back = 1,
-		water_damage = 1,
 		lava_damage = 5,
 		fall_damage = 10,
-		passive = true,
-		view_range = 16,
-		walk_velocity = 2,
-		run_velocity = 4,
-		stepheight = 2.1,
-		jump = false,
+		water_damage = 0,
+		--light_damage = 0,
+		--recovery_time = 0.5,
+		--immune_to = {},
+		--blood_amount = 5,
+		--blood_texture = "mobs_blood.png",
+
+		makes_footstep_sound = true,
+		--sounds = {},
+
+		knock_back = 1,
+
 		drops = {
 			{name = "mobs:meat_raw", chance = 1, min = 1, max = 3},
 			{name = "mobs:leather", chance = 1, min = 1, max = 4}
 		},
-		follow = {"group:leaves"},
-		replace_rate = 50,
+
 		replace_what = {"group:leaves"},
 		replace_with = "air",
+		replace_rate = 20,
+		--replace_offset = 0,
+
+		--do_custom = function(self, dtime)
+			--end
+		--custom_attack = function(self, to_attack)
+			--end,
+		--on_blast = funtion(object, damage)
+				--return do_damage, do_knockback, drops
+			--end,
+		--on_die = function(self, pos)
+			--end,
 		on_rightclick = function(self, clicker)
-			if mobs:feed_tame(self, clicker, 10, true) then
-				return
+				if mobs:feed_tame(self, clicker, 10, true, false) then
+					return
+				end
+				mobs:capture_mob(self, clicker, 0, 0, 80, false, nil)
 			end
-			mobs:capture_mob(self, clicker, 0, 0, 80, false, nil)
-		end
 	})
 
 	local l_spawn_elevation_min = minetest.setting_get("water_level")
