450e776f3d7008a197e83fd5033a29986e8c1a38
blert2112 <cdilts@bellsouth.net>
2016-07-19 23:11:23 -0400

    lib_mount function update

--- mobs_horse/init.lua
+++ mobs_horse/init.lua
@@ -92,7 +92,7 @@ if mobs.mod and mobs.mod == "redo" then
 
 		do_custom = function(self, dtime)
 				if self.driver then
-					lib_mount.drive(self, dtime, "walk", "stand", false)
+					lib_mount.drive(self, dtime, "walk", "stand", 1, false)
 					return false
 				end
 				return true
